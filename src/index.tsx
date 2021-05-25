88a2778fe27e7da4fc858324d05fe590296d0bcf
merobi-hub <merobinson@uri.edu>
2021-05-25 11:37:17 -0400

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Home } from './components'
+import './styles.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home title='Operators-61 Car Inventory' />
   </React.StrictMode>,
   document.getElementById('root')
 );
